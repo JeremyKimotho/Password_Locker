9f267f1a1962b1425a7be4d501dd3f4063996496
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-11-02 17:52:31 +0300

    I have now written about half of the main function in the run file. So far it has user login and password entering

--- run.py
+++ run.py
@@ -20,10 +20,105 @@ def password_generator(site, site_username, characters)
   '''
   Function to generate a password for a user
   '''
-  characters='a1Ab2Bc3Cd4De5Ef6Fg7Gh8Hi9Ij0Jk!Km@Mn#No$Op%Pq^Q'
-  new_generated_password = PasswordInfo(site, site_username, )
+  characters.string='a1Ab2Bc3Cd4De5Ef6Fg7Gh8Hi9Ij0Jk!Km@Mn#No$Op%Pq^Q'
+  new_generated_password = PasswordInfo(site, site_username, def random_char(characters):
+       return ''.join(random.choice(characters.string) for x in range(characters)))
 
-  def random_char(y):
-       return ''.join(random.choice(string.ascii_letters) for x in range(y))
+def save_users(user):
+  '''
+  Function to save users
+  '''
+  user.user_save()
+
+def save_passwords(password):
+  '''
+  Function to save passwords
+  '''
+  password.password_save()
+
+def del_user(user):
+  '''
+  Function that deletes a users credentials
+  '''
+  user.user_delete()
+
+def del_password(password):
+  '''
+  Function that deletes a password
+  '''
+
+def copy_password()
+  '''
+  Function that returns the copied password
+  '''
+  return PasswordInfo.copy_password()
+
+def display_passwords():
+  '''
+  Function that returns all saved contacts
+  '''
+  return PasswordInfo.Passwords()
+
+def main():
+  print('Hello Welcome to password locker. To login to your account type Y, to create an account type N')
+    
+    response=input()
+
+    if response == 'Y':
+      print('Please enter your username')
+        username=input()
+      print('Please enter your password')
+        password=input()
+
+        if username in Users and password in Users:
+
+          print(f'Welcome {username}!')
+          print('\n')
+
+          while True:
+            print('Use these short codes: cp-create a new password with generation, cpg-create a new password with password generation, dp-display passwords, ex-exit the password locker.')
+
+            short_code=input().lower()
+
+            if short_code == 'cp':
+            print('What is the site?')
+              site=input()
+            print('What is your username on this site?')
+              site.username=input()
+            print('Enter a password')
+              password_1=input()
+            print('Confirm password')
+              password_2=input()
+
+              if password_1 != password_2: 
+                print('Your passwords did not match')
+                print('Please re-enter your password')
+                  password_1=input()
+                print('Please confirm your password')
+                  password_2=input()
+
+                  save_passwords(site, site. username, password_1)
+              else:
+                save_passwords(site, site.username, password_1)
+
+            elif short_code == 'cpg':
+              print('What is the site?')
+                site=input()
+              print('What is your username on this site?')
+                site.username=input()
+              print('How many characters would you like it to have? (Between 1-14)')
+                characters=input()
+
+                save_passwords(password_generator(site, site.username, characters))
+
+            elif short_code == 'dp':
+
+
+    
+
+
+
+
+  
 
 
--- test_main.py
+++ test_main.py
@@ -3,58 +3,58 @@ from main import UserInfo
 import pyperclip
 from main import PasswordInfo
 
-# class TestUser(unittest.TestCase):
-#   '''
-#   Test class that defines test cases for the contact class behaviours.
+class TestUser(unittest.TestCase):
+  '''
+  Test class that defines test cases for the contact class behaviours.
 
-#   Args:
-#   unittest.TestCase: TestCase class that helps in creating test cases
-#   '''
+  Args:
+  unittest.TestCase: TestCase class that helps in creating test cases
+  '''
 
-#   def setUp(self):
-#     '''
-#     This is a set up method to run before each test case.
-#     '''
-#     self.new_user = UserInfo('Jeremy', '12345')
+  def setUp(self):
+    '''
+    This is a set up method to run before each test case.
+    '''
+    self.new_user = UserInfo('Jeremy', '12345')
   
-#   def tearDown(self):
-#     '''
-#     That erases the data after each test case has run.
-#     '''
-#     UserInfo.Users=[]
+  def tearDown(self):
+    '''
+    That erases the data after each test case has run.
+    '''
+    UserInfo.Users=[]
 
-#   def test_init(self):
-#     '''
-#     test case to see if the object is correctly initialised.
-#     '''
-#     self.assertEqual(self.new_user.username,'Jeremy')
-#     self.assertEqual(self.new_user.password,'12345')
+  def test_init(self):
+    '''
+    test case to see if the object is correctly initialised.
+    '''
+    self.assertEqual(self.new_user.username,'Jeremy')
+    self.assertEqual(self.new_user.password,'12345')
 
-#   def test_user_save(self):
-#     '''
-#     this test case will be testing if saving a users username and password is possible and done into the user object
-#     '''
-#     self.new_user.user_save()
-#     self.assertEqual(len(UserInfo.Users),1)
+  def test_user_save(self):
+    '''
+    this test case will be testing if saving a users username and password is possible and done into the user object
+    '''
+    self.new_user.user_save()
+    self.assertEqual(len(UserInfo.Users),1)
 
-#   def test_multiple_user_save(self):
-#     '''
-#     this test case will be used to test if multiple users can be saved if need be 
-#     '''
-#     self.new_user.user_save()
-#     test_user = UserInfo('Test', 'Tester')
-#     test_user.user_save()
-#     self.assertEqual(len(UserInfo.Users),2)
+  def test_multiple_user_save(self):
+    '''
+    this test case will be used to test if multiple users can be saved if need be 
+    '''
+    self.new_user.user_save()
+    test_user = UserInfo('Test', 'Tester')
+    test_user.user_save()
+    self.assertEqual(len(UserInfo.Users),2)
 
-#   def test_delete_user(self):
-#     '''
-#     this test case to make sure a user can be deleted
-#     '''
-#     self.new_user.user_save()
-#     test_user = UserInfo('Test', 'Tester')
-#     test_user.user_save()
-#     self.new_user.user_delete()
-#     self.assertEqual(len(UserInfo.Users),1)
+  def test_delete_user(self):
+    '''
+    this test case to make sure a user can be deleted
+    '''
+    self.new_user.user_save()
+    test_user = UserInfo('Test', 'Tester')
+    test_user.user_save()
+    self.new_user.user_delete()
+    self.assertEqual(len(UserInfo.Users),1)
 
 class TestPassword(unittest.TestCase):
   '''
